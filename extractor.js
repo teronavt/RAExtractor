1678c143705e3566508cad60c1b48e67a0e295ce
teronavt <aeeneon@gmail.com>
2019-11-22 22:46:22 +0300

    Update extractor.js

--- extractor.js
+++ extractor.js
@@ -1,6 +1,6 @@
 function extract()
 {
     var files = document.getElementById("fileinput").files;
-    var sfile = files.size;
+    var sfile = files[0].size;
     document.getElementById("fileSize").innerHTML = sfile;
 }
